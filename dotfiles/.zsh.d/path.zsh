b774a3681e023bec5f0b8665fa3446b659c8d785
Masatomo Toyama <masa.70.fg@gmail.com>
2021-10-19 00:17:25 +0900

    update zsh path file

--- dotfiles/.zsh.d/path.zsh
+++ dotfiles/.zsh.d/path.zsh
@@ -1,12 +1,13 @@
+export LDFLAGS="-L/usr/local/opt/zlib/lib"
+export CPPFLAGS="-I/usr/local/opt/zlib/include"
+
 export PATH="$PATH:/usr/local/bin:/usr/local/sbin"
 export PATH="$PATH:$GOPATH/bin"
 export PGDATA=/usr/local/var/postgres
 
-# nvm
-export NVM_DIR="$HOME/.nvm/bin:$PATH"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-source $(brew --prefix nvm)/nvm.sh
-export PATH=$PATH:~/.local/bin
+# anyenv
+export PATH="$HOME/.anyenv/bin:$PATH"
+eval "$(anyenv init -)"
 
 # goenv
 export PATH="$HOME/.goenv/bin:$PATH"
@@ -25,18 +26,19 @@ if [ -d ${HOME}/node_modules/.bin ]; then
 fi
 
 # android
-export ANDROID_HOME=$HOME/Library/Android/sdk
+export ANDROID_HOME="$HOME/Library/Android/sdk"
+export PATH=$PATH:$ANDROID_HOME/emulator
 export PATH=$PATH:$ANDROID_HOME/tools
+export PATH=$PATH:$ANDROID_HOME/tools/bin
 export PATH=$PATH:$ANDROID_HOME/platform-tools
 
 # stack
 export PATH="$HOME/.stack/bin:$PATH"
 
-export PATH=$HOME/.nodebrew/current/bin:$PATH
-
+# export PATH=$HOME/.nodebrew/current/bin:$PATH
 # pipenv
-export PIPENV_VENV_IN_PROJECT=true
-eval "$(pipenv --completion)"
+# export PIPENV_VENV_IN_PROJECT=true
+# eval "$(pipenv --completion)"
 
 # pyenv
 export PYENV_ROOT="$HOME/.pyenv"
@@ -44,5 +46,10 @@ export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 
 # php
-export PATH="/usr/local/opt/php@7.2/bin:$PATH"
-export PATH="/usr/local/opt/php@7.2/sbin:$PATH"
+#export PATH="/usr/local/opt/php@7.2/bin:$PATH"
+#export PATH="/usr/local/opt/php@7.2/sbin:$PATH"
+export PATH="/Applications/Julia-1.5.app/Contents/Resources/julia/bin:$PATH"
+
+export PATH="/usr/local/opt/openjdk@11/bin:$PATH"
+export JAVA_HOME=`/usr/libexec/java_home -v 11`
+
