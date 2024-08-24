56fcba5a73948842f35d0217e89e4a94afc8067c
Masatomo Toyama <masa.70.fg@gmail.com>
2024-08-24 16:30:11 +0900

    update path.zsh

--- dotfiles/.zsh.d/path.zsh
+++ dotfiles/.zsh.d/path.zsh
@@ -10,42 +10,25 @@ export GOBIN=$GOPATH/bin
 
 export PATH=$PATH:/usr/local/Cellar/libpq/14.3/bin/:~/bin/
 
-# anyenv
-export PATH="$HOME/.anyenv/bin:$PATH"
-eval "$(anyenv init -)"
+# rust
+export PATH="$HOME/.cargo/bin:$PATH"
 
 # rbebv
 export PATH="$HOME/.rbenv/bin:$PATH" 
 eval "$(rbenv init - zsh)"
-
-# added by travis gem
-[ -f $HOME/.travis/travis.sh ] && source $HOME/.travis/travis.sh
 
 # node
 if [ -d ${HOME}/node_modules/.bin ]; then
   export PATH=${PATH}:${HOME}/node_modules/.bin
 fi
 
-# android
-export ANDROID_HOME="$HOME/Library/Android/sdk"
-export PATH=$PATH:$ANDROID_HOME/emulator
-export PATH=$PATH:$ANDROID_HOME/tools
-export PATH=$PATH:$ANDROID_HOME/tools/bin
-export PATH=$PATH:$ANDROID_HOME/platform-tools
+# nvim
+export NVM_DIR="/Users/masatomotoyama/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
-# stack
-export PATH="$HOME/.stack/bin:$PATH"
+# direnv
+eval "$(direnv hook zsh)"
 
-# export PATH=$HOME/.nodebrew/current/bin:$PATH
-# pipenv
-# export PIPENV_VENV_IN_PROJECT=true
-# eval "$(pipenv --completion)"
-
-# php
-#export PATH="/usr/local/opt/php@7.2/bin:$PATH"
-#export PATH="/usr/local/opt/php@7.2/sbin:$PATH"
-export PATH="/Applications/Julia-1.5.app/Contents/Resources/julia/bin:$PATH"
-
-export PATH="/usr/local/opt/openjdk@11/bin:$PATH"
-export JAVA_HOME=`/usr/libexec/java_home -v 11`
+# font
+export TERM=xterm-256color
 
